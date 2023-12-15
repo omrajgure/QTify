e7015428ac49d8b4c61f1dbca7de38214c6e2c25
om <om22rajgure@gmail.com>
2023-12-15 21:46:30 +0530

    C3:Button

---
+++ src/components/feedback_comp.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Feedback = () => {
+  return (
+    <div>
+      <button
+        style={{
+          padding: "10px 16px 10px 16px",
+          color: "#34C94B",
+          backgroundColor: "#121212",
+          borderRadius: "12px",
+          height: "47px",
+          width: "170px",
+          gap: "10px",
+          border: "1px",
+        }}
+      >
+        Give Feedback
+      </button>
+    </div>
+  );
+};
