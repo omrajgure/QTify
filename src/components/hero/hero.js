5f22354891a4b96fa720376f442e47dd5d74218c
om <om22rajgure@gmail.com>
2023-12-19 17:11:48 +0530

    hero_section updated

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -5,14 +5,12 @@ import img from "../../assets/vibrating-headphone 1.png";
 export const Hero = () => {
   return (
     <div className={styles.hero_div}>
-      <div className={styles.hero_inner_splitdiv}>
-        <div className={styles.hero_txt}>
-          <p>100 Thousand Songs, ad-free</p>
-          <p>Over thousands podcast episodes</p>
-        </div>
-        <div>
-          <img src={img} />
-        </div>
+      <div>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousands podcast episodes</h1>
+      </div>
+      <div>
+        <img src={img} width={212} height={212} />
       </div>
     </div>
   );
